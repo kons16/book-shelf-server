e5c920b1935f6cca7f443bf001d2ce3d64f9e483
kons16 <ks1216r@gmail.com>
2021-01-04 13:25:29 +0900

    go関連のファイルを追加

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
-module github.com/kons16/book-shlef-server
+module github.com/kons16/book-shelf-server
 
 go 1.14
+
+require (
+	github.com/go-sql-driver/mysql v1.5.0
+	github.com/jmoiron/sqlx v1.2.0
+	github.com/joho/godotenv v1.3.0
+)
--- infra/mysql/infra.go
+++ infra/mysql/infra.go
@@ -1,1 +1,36 @@
 package mysql
+
+import (
+	"fmt"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+	"github.com/joho/godotenv"
+)
+
+func NewMySQLDB() (*sqlx.DB, error) {
+	if err := godotenv.Load(); err != nil {
+		return nil, err
+	}
+
+	connectionString := fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		os.Getenv("DB_USER_NAME"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_IP_ADDRESS"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+	)
+
+	db, err := sqlx.Connect("mysql", connectionString)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open MySQL: %w", err)
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, fmt.Errorf("failed to ping: %w", err)
+	}
+
+	return db, nil
+}
