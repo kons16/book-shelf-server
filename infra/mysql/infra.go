3773f74b22e6fcd60600fcb816973ea9d8344a02
kons16 <ks1216r@gmail.com>
2021-01-04 13:33:36 +0900

    mysqlからMySQLに名前変更

--- infra/mysql/infra.go
+++ infra/mysql/infra.go
@@ -1,4 +1,4 @@
-package mysql
+package MySQL
 
 import (
 	"fmt"
--- main.go
+++ main.go
@@ -1,4 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/kons16/book-shelf-server/infra/MySQL"
+)
+
 func main() {
+	_, err := MySQL.NewMySQLDB()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(0)
+	}
 }
